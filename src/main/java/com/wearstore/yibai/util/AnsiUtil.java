93f0dcc84a2b3c0728cf7c644395eb7302fff670
tianpengfei <997212845@qq.com>
2021-08-18 17:03:32 +0800

    项目启动时的控制台输出信息修改

---
+++ src/main/java/com/wearstore/yibai/util/AnsiUtil.java
@@ -0,0 +1,42 @@
+package com.wearstore.yibai.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.fusesource.jansi.Ansi;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author PF.Tian
+ * @since 2021/08/18
+ */
+@Slf4j
+public class AnsiUtil {
+
+    private static boolean enableAnsi;
+
+    static {
+        Boolean value = false;
+        try {
+            Environment environment = SpringContextUtil.getBean(Environment.class);
+            value = environment.getProperty("wear-store.enable-ansi",boolean.class);
+            value = value == null ? false : value;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        enableAnsi = value;
+    }
+
+    public static String getAnsi(Ansi.Color color, String text){
+
+        if (enableAnsi){
+            return Ansi.ansi().eraseScreen().fg(color).a(text).reset().toString();
+        }
+        return text;
+    }
+
+    public static String getAnsi(Ansi.Color color,String text,boolean flag){
+        if (flag){
+            return Ansi.ansi().eraseScreen().fg(color).a(text).reset().toString();
+        }
+        return text;
+    }
+}
